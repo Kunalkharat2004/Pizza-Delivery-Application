d975777f2c8d5250a2bc4d23f2825734e02f5a67
Kunal <kunalkharat2004@gmail.com>
2024-12-28 21:55:11 +0530

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        ignores: ["dist", "node_modules", "eslint.config.mjs"],
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        rules: {
+            // "dot-notation": "error",
+            "no-unused-vars": "warn",
+            "no-console": "error",
+            "@typescript-eslint/no-misused-promises": "off",
+        },
+    }
+);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const student = {
+    name: 'John Doe',
+    age: 25,
+    course: 'Computer Science',
+    address: {
+        city: 'Lagos',
+        country: 'Nigeria'
+    }
+}
+
+const studentName = student.name;

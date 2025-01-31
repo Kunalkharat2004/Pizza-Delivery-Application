8a9144aa934ae76b2fccfb097b9d733fa1ca4e42
Kunal <kunalkharat2004@gmail.com>
2025-01-31 22:29:35 +0530

    format generate-private-public-key.mjs

--- scripts/generate-private-public-key.mjs
+++ scripts/generate-private-public-key.mjs
@@ -3,23 +3,23 @@ import * as fs from "fs";
 import logger from "../src/config/logger";
 
 try {
-    const { privateKey, publicKey } = generateKeyPairSync("rsa", {
-        modulusLength: 2048,
-        publicKeyEncoding: {
-            type: "spki",
-            format: "pem",
-        },
-        privateKeyEncoding: {
-            type: "pkcs8",
-            format: "pem",
-        }
-    });
+  const { privateKey, publicKey } = generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+    publicKeyEncoding: {
+      type: "spki",
+      format: "pem",
+    },
+    privateKeyEncoding: {
+      type: "pkcs8",
+      format: "pem",
+    },
+  });
 
-    // Ensure the "certs" directory exists
-    fs.mkdirSync("certs", { recursive: true });
+  // Ensure the "certs" directory exists
+  fs.mkdirSync("certs", { recursive: true });
 
-    fs.writeFileSync("certs/private.pem", privateKey);
-    fs.writeFileSync("certs/public.pem", publicKey);
+  fs.writeFileSync("certs/private.pem", privateKey);
+  fs.writeFileSync("certs/public.pem", publicKey);
 } catch (error) {
-    logger.error(error);
+  logger.error(error);
 }

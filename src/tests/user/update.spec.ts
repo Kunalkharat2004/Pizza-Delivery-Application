4c52ddee6a3e130356deeede4d4d82c80cf163a0
Kunal <kunalkharat2004@gmail.com>
2025-02-26 13:31:25 +0530

    refactor update test

--- src/tests/user/update.spec.ts
+++ src/tests/user/update.spec.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import app from "../../app";
 import { DataSource } from "typeorm";
 import { AppDataSource } from "../../config/data-source";
-import { ITenant } from "../../types";
+import { ITenant, IUser } from "../../types";
 import { createJWKSMock, JWKSMock } from "mock-jwks";
 import { Roles } from "../../constants";
 
@@ -50,51 +50,42 @@ describe("PUT /users/:id", () => {
       expect(response1.statusCode).toBe(401);
       stopJwks();
     });
-    // it("should return 403 if user is not admin", async () => {
-    //   // Arrange
-    //   const tenantData = {
-    //     name: "Raj Sweet Shop",
-    //     address: "Pune, India",
-    //   };
-    //   jwksMock = createJWKSMock("http://localhost:3200");
-    //   stopJwks = jwksMock.start();
-    //   adminToken = jwksMock.token({
-    //     sub: "1234567890",
-    //     role: Roles.MANAGER,
-    //   });
-    //   const response = await request(app).post("/tenant").set("Cookie", `accessToken=${adminToken}`).send(tenantData);
-    //   const tenantId = (response.body as ITenant).id;
-    //   // Act
-    //   const response1 = await request(app)
-    //     .put(`/tenant/${tenantId}`)
-    //     .set("Cookie", `accessToken=${adminToken}`)
-    //     .send({ name: "Rajesh Sweet Shop", address: "Pune, India" });
-    //   // Assert
-    //   expect(response1.statusCode).toBe(403);
-    //   stopJwks();
-    // });
-    // it("should return 200 status code", async () => {
-    //   // Arrange
-    //   jwksMock = createJWKSMock("http://localhost:3200");
-    //   stopJwks = jwksMock.start();
-    //   adminToken = jwksMock.token({
-    //     sub: "1234567890",
-    //     role: Roles.ADMIN,
-    //   });
-    //   const tenantData = {
-    //     name: "Raj Sweet Shop",
-    //     address: "Pune, India",
-    //   };
-    //   const response = await request(app).post("/tenant").set("Cookie", `accessToken=${adminToken}`).send(tenantData);
-    //   const tenantId = (response.body as ITenant).id;
-    //   // Act
-    //   const response1 = await request(app)
-    //     .put(`/tenant/${tenantId}`)
-    //     .set("Cookie", `accessToken=${adminToken}`)
-    //     .send({ name: "Rajesh Sweet Shop", address: "Pune, India" });
-    //   // Assert
-    //   expect(response1.statusCode).toBe(200);
-    //   stopJwks();
-    // });
+    it("should return 403 if user is not admin", async () => {
+      jwksMock = createJWKSMock("http://localhost:3200");
+      stopJwks = jwksMock.start();
+      adminToken = jwksMock.token({
+        sub: "1234567890",
+        role: Roles.MANAGER,
+      });
+      const response = await request(app).post("/users").set("Cookie", `accessToken=${adminToken}`).send(managerData);
+      const userId = (response.body as IUser).id;
+      // Act
+      const response1 = await request(app)
+        .put(`/users/${userId}`)
+        .set("Cookie", `accessToken=${adminToken}`)
+        .send({ ...managerData, firstName: "Rajesh", address: "Pune, India" });
+      // Assert
+      expect(response1.statusCode).toBe(403);
+      stopJwks();
+    });
+    it("should return 200 status code", async () => {
+      // Arrange
+      jwksMock = createJWKSMock("http://localhost:3200");
+      stopJwks = jwksMock.start();
+      adminToken = jwksMock.token({
+        sub: "1234567890",
+        role: Roles.ADMIN,
+      });
+      const response = await request(app).post("/users").set("Cookie", `accessToken=${adminToken}`).send(managerData);
+      const userId = (response.body as ITenant).id;
+      // Act
+      const response1 = await request(app)
+        .put(`/users/${userId}`)
+        .set("Cookie", `accessToken=${adminToken}`)
+        .send({ ...managerData, firstName: "Rajesh", address: "Pune, India" });
+      // Assert
+      expect(response1.statusCode).toBe(200);
+      stopJwks();
+    });
   });
 });

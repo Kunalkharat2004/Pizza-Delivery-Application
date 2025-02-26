6e5ba57d3261e785faac7cb56fbe04a44ca11148
Kunal <kunalkharat2004@gmail.com>
2025-02-26 12:02:03 +0530

    add test for validating the fields of user

--- src/tests/user/create.spec.ts
+++ src/tests/user/create.spec.ts
@@ -13,7 +13,7 @@ describe("POST /users", () => {
     firstName: "Shraddha",
     lastName: "Pawar",
     email: "shraddha@gmail.com",
-    password: "shraddha$123",
+    password: "Shraddha$123",
     address: "Bangalore, India",
   };
 
@@ -114,91 +114,176 @@ describe("POST /users", () => {
       const users = await connection.getRepository(User).find();
       expect(response.statusCode).toBe(400);
       expect(response.body).toHaveProperty("errors");
+      expect(users).toHaveLength(0);
 
       (response.body as AuthResponse).errors.forEach((error: { msg: string }) => {
         expect(error).toHaveProperty("msg");
         expect(error.msg).toBeDefined();
-        expect(users).toHaveLength(0);
       });
     });
 
-    // it("should return 400 statusCode if password is missing", async () => {
-    //   // Arrange
-    //   const user = {
-    //     firstName: "Kunal",
-    //     lastName: "Kharat",
-    //     email: "kunalkharat2004@gmail.com",
-    //     password: "",
-    //     address: "Pune, India",
-    //   };
+    it("should return 400 statusCode if password is missing", async () => {
+      const adminToken = jwksMock.token({
+        sub: "123",
+        role: Roles.ADMIN,
+      });
 
-    //   // Act
-    //   const response = await request(app).post("/auth/register").send(user);
+      // Act
+      const response = await request(app)
+        .post("/users")
+        .set("Cookie", `accessToken=${adminToken}`)
+        .send({ ...managerData, password: "" });
 
-    //   const users = await connection.getRepository(User).find();
-    //   // Assert
-    //   expect(response.statusCode).toBe(400);
-    //   expect(response.body).toHaveProperty("errors");
-    //   expect(users).toHaveLength(0);
-    // });
+      const users = await connection.getRepository(User).find();
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(users).toHaveLength(0);
 
-    // it("should return 400 statusCode if firstName is missing", async () => {
-    //   // Arrange
-    //   const user = {
-    //     firstName: "",
-    //     lastName: "Kharat",
-    //     email: "kunalkharat2004@gmail.com",
-    //     password: "secret@123",
-    //     address: "Pune, India",
-    //   };
+      (response.body as AuthResponse).errors.forEach((error: { msg: string }) => {
+        expect(error).toHaveProperty("msg");
+        expect(error.msg).toBeDefined();
+      });
+    });
+    it("should return 400 statusCode if firstName is missing", async () => {
+      const adminToken = jwksMock.token({
+        sub: "123",
+        role: Roles.ADMIN,
+      });
 
-    //   // Act
-    //   const response = await request(app).post("/auth/register").send(user);
+      // Act
+      const response = await request(app)
+        .post("/users")
+        .set("Cookie", `accessToken=${adminToken}`)
+        .send({ ...managerData, firstName: "" });
 
-    //   const users = await connection.getRepository(User).find();
-    //   // Assert
+      const users = await connection.getRepository(User).find();
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(users).toHaveLength(0);
 
-    //   expect(response.statusCode).toBe(400);
-    //   expect(response.body).toHaveProperty("errors");
-    //   expect(users).toHaveLength(0);
-    // });
-    // it("should return 400 statusCode if lastName is missing", async () => {
-    //   // Arrange
-    //   const user = {
-    //     firstName: "Kunal",
-    //     lastName: "",
-    //     email: "kunalkharat2004@gmail.com",
-    //     password: "secret@123",
-    //     address: "Pune, India",
-    //   };
+      (response.body as AuthResponse).errors.forEach((error: { msg: string }) => {
+        expect(error).toHaveProperty("msg");
+        expect(error.msg).toBeDefined();
+      });
+    });
+    it("should return 400 statusCode if lastName is missing", async () => {
+      const adminToken = jwksMock.token({
+        sub: "123",
+        role: Roles.ADMIN,
+      });
 
-    //   // Act
-    //   const response = await request(app).post("/auth/register").send(user);
+      // Act
+      const response = await request(app)
+        .post("/users")
+        .set("Cookie", `accessToken=${adminToken}`)
+        .send({ ...managerData, lastName: "" });
 
-    //   const users = await connection.getRepository(User).find();
-    //   // Assert
-    //   expect(response.statusCode).toBe(400);
-    //   expect(response.body).toHaveProperty("errors");
-    //   expect(users).toHaveLength(0);
-    // });
-    // it("should return 400 statusCode if address is missing", async () => {
-    //   // Arrange
-    //   const user = {
-    //     firstName: "Kunal",
-    //     lastName: "Kharat",
-    //     email: "kunalkharat2004@gmail.com",
-    //     password: "secret@123",
-    //     address: "",
-    //   };
+      const users = await connection.getRepository(User).find();
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(users).toHaveLength(0);
 
-    //   // Act
-    //   const response = await request(app).post("/auth/register").send(user);
+      (response.body as AuthResponse).errors.forEach((error: { msg: string }) => {
+        expect(error).toHaveProperty("msg");
+        expect(error.msg).toBeDefined();
+      });
+    });
+    it("should return 400 statusCode if address is missing", async () => {
+      const adminToken = jwksMock.token({
+        sub: "123",
+        role: Roles.ADMIN,
+      });
 
-    //   const users = await connection.getRepository(User).find();
-    //   // Assert
-    //   expect(response.statusCode).toBe(400);
-    //   expect(response.body).toHaveProperty("errors");
-    //   expect(users).toHaveLength(0);
-    // });
+      // Act
+      const response = await request(app)
+        .post("/users")
+        .set("Cookie", `accessToken=${adminToken}`)
+        .send({ ...managerData, address: "" });
+
+      const users = await connection.getRepository(User).find();
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(users).toHaveLength(0);
+
+      (response.body as AuthResponse).errors.forEach((error: { msg: string }) => {
+        expect(error).toHaveProperty("msg");
+        expect(error.msg).toBeDefined();
+      });
+    });
+  });
+  describe("Fields not in proper format", () => {
+    it("should return 400 statusCode if email is not in proper format", async () => {
+      const adminToken = jwksMock.token({
+        sub: "123",
+        role: Roles.ADMIN,
+      });
+
+      // Act
+      const response = await request(app)
+        .post("/users")
+        .set("Cookie", `accessToken=${adminToken}`)
+        .send({ ...managerData, email: "shraddha" });
+
+      const users = await connection.getRepository(User).find();
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(users).toHaveLength(0);
+
+      (response.body as AuthResponse).errors.forEach((error: { msg: string }) => {
+        expect(error).toHaveProperty("msg");
+        expect(error.msg).toBeDefined();
+      });
+    });
+    it("should return 400 if password lenght is less than 8 characters", async () => {
+      const adminToken = jwksMock.token({
+        sub: "123",
+        role: Roles.ADMIN,
+      });
+
+      // Act
+      const response = await request(app)
+        .post("/users")
+        .set("Cookie", `accessToken=${adminToken}`)
+        .send({ ...managerData, password: "secret" });
+
+      const users = await connection.getRepository(User).find();
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(users).toHaveLength(0);
+
+      (response.body as AuthResponse).errors.forEach((error: { msg: string }) => {
+        expect(error).toHaveProperty("msg");
+        expect(error.msg).toBeDefined();
+      });
+    });
+    it("should return 400 if password doesn't contain atleast 1 uppercase character,1 lower case character, 1 special character and 1 number", async () => {
+      const adminToken = jwksMock.token({
+        sub: "123",
+        role: Roles.ADMIN,
+      });
+
+      // Act
+      const response = await request(app)
+        .post("/users")
+        .set("Cookie", `accessToken=${adminToken}`)
+        .send({ ...managerData, password: "shraddhapawar" });
+
+      const users = await connection.getRepository(User).find();
+      // Assert
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(users).toHaveLength(0);
+
+      (response.body as AuthResponse).errors.forEach((error: { msg: string }) => {
+        expect(error).toHaveProperty("msg");
+        expect(error.msg).toBeDefined();
+      });
+    });
   });
 });

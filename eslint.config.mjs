87b57a0aba368ad5a511d63ce34c2e3acdb9e539
Kunal <kunalkharat2004@gmail.com>
2025-03-05 22:40:47 +0530

    setup test coverage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ export default tseslint.config(
       "node_modules",
       "eslint.config.mjs",
       "jest.config.js",      
-      "*.spec.ts",
+      "src/tests/**/*",
       "src/tests",
     ],
   },
--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,11 @@ module.exports = {
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
+  collectCoverage: true,
+  coverageProvider: "v8",
+  collectCoverageFrom:[
+    "src/**/*.ts",
+    "!src/tests/**",
+    "!**/node_modules/**",
+  ]
 };

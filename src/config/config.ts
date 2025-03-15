b12ab3fbf8ec81f36088509932af65ee931ed4a4
Kunal <kunalkharat2004@gmail.com>
2025-03-15 18:19:54 +0530

    refactor: remove admin user creation logic and related environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -14,11 +14,6 @@ const {
   JWKS_URI,
   PRIVATE_KEY,
   CLIENT_URL,
-  ADMIN_EMAIL,
-  ADMIN_PASSWORD,
-  ADMIN_ADDRESS,
-  ADMIN_FIRSTNAME,
-  ADMIN_LASTNAME,
 } = process.env;
 
 const _config = {
@@ -33,11 +28,6 @@ const _config = {
   JWKS_URI,
   PRIVATE_KEY,
   CLIENT_URL,
-  ADMIN_EMAIL,
-  ADMIN_PASSWORD,
-  ADMIN_ADDRESS,
-  ADMIN_FIRSTNAME,
-  ADMIN_LASTNAME,
 };
 
 const config = Object.freeze(_config);
